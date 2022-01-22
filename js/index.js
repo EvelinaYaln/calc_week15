2525f330e9b805db6c1c00f9e97ab5a072cefaf9
EvelinaYaln <evelina.dranets@gmail.com>
2022-01-22 10:50:59 +0300

    add change in js

--- js/index.js
+++ js/index.js
@@ -2,12 +2,16 @@ calcSum = () => {
     let firstNumber = document.getElementById('first_num').value;
     let secondNumber = document.getElementById('second_num').value;
     let sum = (+firstNumber + +secondNumber);
+    document.getElementById('first_num').value = '';
+    document.getElementById('second_num').value = '';
     document.getElementById('result').value = sum;
 }
 
 calcDiff = () => {
     let firstNumber = document.getElementById('first_num').value;
     let secondNumber = document.getElementById('second_num').value;
+    document.getElementById('first_num').value = '';
+    document.getElementById('second_num').value = '';
     let diff = (+firstNumber - +secondNumber);
     document.getElementById('result').value = diff;
 }
@@ -15,12 +19,16 @@ calcDiff = () => {
 calcMultipl = () => {
     let firstNumber = document.getElementById('first_num').value;
     let secondNumber = document.getElementById('second_num').value;
+    document.getElementById('first_num').value = '';
+    document.getElementById('second_num').value = '';
     let multiple = (+firstNumber * +secondNumber);
     document.getElementById('result').value = multiple;
 }
 calcPortion = () => {
     let firstNumber = document.getElementById('first_num').value;
     let secondNumber = document.getElementById('second_num').value;
+    document.getElementById('first_num').value = '';
+    document.getElementById('second_num').value = '';
     let portion = (+firstNumber / +secondNumber);
     if (+secondNumber === 0) {
         document.getElementById('result').value = "На ноль делить нельзя!";
