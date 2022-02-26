237ae6854271e7c101bd45fa70a117d262d32e74
EvelinaYaln <evelina.dranets@gmail.com>
2022-02-26 13:27:02 +0300

    create static methods

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-calcSum = () => {
+/*calcSum = () => {
     let firstNumber = document.getElementById('first_num').value;
     let secondNumber = document.getElementById('second_num').value;
     let sum = (+firstNumber + +secondNumber);
@@ -36,4 +36,43 @@ calcPortion = () => {
         document.getElementById('result').value = portion;
     }
 
+}*/
+
+class Calculator {
+    static calcSum(firstNumber, secondNumber) {
+        document.getElementById('first_num').value = '';
+        document.getElementById('second_num').value = '';
+        let sum = (+firstNumber + +secondNumber);
+        document.getElementById('result').value = sum;
+        return sum;
+    }
+    static calcDiff(firstNumber, secondNumber) {
+        document.getElementById('first_num').value = '';
+        document.getElementById('second_num').value = '';
+        let diff = (+firstNumber - +secondNumber);
+        document.getElementById('result').value = diff;
+        return diff;
+    }
+    static calcMultipl(firstNumber, secondNumber) {
+        document.getElementById('first_num').value = '';
+        document.getElementById('second_num').value = '';
+        let multiple = (+firstNumber * +secondNumber);
+        document.getElementById('result').value = multiple;
+    }
+    static calcPortion(firstNumber, secondNumber) {
+        document.getElementById('first_num').value = '';
+        document.getElementById('second_num').value = '';
+        let portion = (+firstNumber / +secondNumber);
+        if (+secondNumber === 0) {
+            document.getElementById('result').value = "На ноль делить нельзя!";
+        } else {
+            document.getElementById('result').value = portion;
+        }
+    }
 }
+//let firstNumber = document.getElementById('first_num').value;
+//let secondNumber = document.getElementById('second_num').value;
+sum.addEventListener('click', () => { Calculator.calcSum(document.getElementById('first_num').value, document.getElementById('second_num').value) });
+diff.addEventListener('click', () => { Calculator.calcDiff(document.getElementById('first_num').value, document.getElementById('second_num').value) });
+multiple.addEventListener('click', () => { Calculator.calcMultipl(document.getElementById('first_num').value, document.getElementById('second_num').value) });
+portion.addEventListener('click', () => { Calculator.calcPortion(document.getElementById('first_num').value, document.getElementById('second_num').value) });
